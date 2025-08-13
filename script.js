8a082ee34b26c4a565d779e33212907fbe195bfa
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-13 15:16:23 +0530

    Built DOM to render books in table

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 const uuidBrowser = crypto.randomUUID();
 
+const container = document.querySelector("#container");
+const table = document.querySelector("table");
+
 const allBooks = [];
 
-function Book(title, author, numOfPages, read) {
+function Book(title, author, numOfPages) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.numOfPages = numOfPages;
-  this.read = read;
 }
 
-function createBook(title, author, numOfPages, read) {
-  return allBooks.push(new Book(title, author, numOfPages, read));
+function createBook(title, author, numOfPages) {
+  return allBooks.push(new Book(title, author, numOfPages));
 }
 
-let bookOne = createBook("Harry Books", "Rowlings", 500, "Read the book");
-let bookTwo = createBook("Ron Books", "Jake", 250, "Haven't read the book");
-let bookThree = createBook("Hermoine Books", "Robert", 754, "Read the book");
+function displayBooks() {
+  allBooks.forEach((book) => {
+    const tableRow = document.createElement("tr");
+    const titleCell = document.createElement("td");
 
-console.log(allBooks);
+    titleCell.textContent = `${book.title}`;
+
+    const authorCell = document.createElement("td");
+    authorCell.textContent = `${book.author}`;
+
+    const pagesCell = document.createElement("td");
+    pagesCell.textContent = `${book.numOfPages}`;
+
+    table.appendChild(tableRow);
+    tableRow.appendChild(titleCell);
+    tableRow.appendChild(authorCell);
+    tableRow.appendChild(pagesCell);
+  });
+}
+
+let bookOne = createBook("Harry Books", "Rowlings", 500);
+let bookTwo = createBook("Ron Books", "Jake", 250);
+let bookThree = createBook("Hermoine Books", "Robert", 754);
+
+displayBooks();
