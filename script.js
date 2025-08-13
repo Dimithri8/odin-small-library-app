cef7dc48c27366b6a15d206262456225d2817a45
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-13 16:29:19 +0530

    Developed logic to add new books to table when form is submitted

--- script.js
+++ script.js
@@ -2,6 +2,36 @@ const uuidBrowser = crypto.randomUUID();
 
 const container = document.querySelector("#container");
 const table = document.querySelector("table");
+const openPopup = document.querySelector(".open-popup");
+const booksDialog = document.querySelector("#add-books-dialog");
+const closeFormBtn = document.querySelector(".close-form-btn");
+const addBookBtn = document.querySelector(".form-add-btn");
+const form = document.querySelector("form");
+
+openPopup.addEventListener("click", () => {
+  booksDialog.style.position = "relative";
+  booksDialog.style.top = "100px";
+  booksDialog.showModal();
+});
+
+closeFormBtn.addEventListener("click", () => {
+  booksDialog.close();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const numOfPages = parseInt(formData.get("numOfPages"));
+
+  createBook(title, author, numOfPages);
+  form.reset();
+  displayBooks();
+
+  booksDialog.close();
+});
 
 const allBooks = [];
 
@@ -17,6 +47,8 @@ function createBook(title, author, numOfPages) {
 }
 
 function displayBooks() {
+  table.querySelectorAll("tr:not(:first-child)").forEach((row) => row.remove());
+
   allBooks.forEach((book) => {
     const tableRow = document.createElement("tr");
     const titleCell = document.createElement("td");
